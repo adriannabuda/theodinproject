d9bbe10b8a8ab4ea94a12f25c06bef7fd28083bd
adriannabuda <32776262+adriannabuda@users.noreply.github.com>
2020-06-26 10:44:05 +0200

    Update caesar_cipher.rb
    ASCII added (instead of regular alphabet)

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,20 +1,30 @@
-def caesar_cipher (string, number)
-    caesar_string = ""
-    lower_case = ("a"..."z").to_a
+def caesar_cipher(string, shift)
+  
+  caesar_string=""
+  alphabet= string.split(//)
+  alphabet.each do |x| 
+  number = x.ord 
 
-    string.each_char do |i|
-      if lower_case.include? (i.downcase) 
-        number.times {i = i.next}
+    if
+      number.between?(97,122)# A-Z?
+        number = number + shift
+        if number >122 # if result is past Z
+          number = (number -122) %26 + 96 
+        end
+    elsif number.between?(65,90)
+      number = number + shift
+      if number >90
+        number = (number-90) %26 + 64
       end
-
-      caesar_string << i[-1]
     end
-    return caesar_string
+    caesar_string<<number.chr
   end
+caesar_string
+end
 
-  print "What would you like to encrypt?"
-  text = gets.chomp
-  print "With shift of:"
-  number = gets.chomp.to_i
+puts "What would you like to encrypt?"
+string= gets.chomp
+puts "With shift of:"
+shift= gets.chomp.to_i
 
-  puts caesar_cipher(text,number)
+caesar_cipher(string, shift)
