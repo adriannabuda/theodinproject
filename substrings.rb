1230fb95c23bdfc2722a32964d2a8df7e11969f3
adriannabuda <32776262+adriannabuda@users.noreply.github.com>
2020-06-28 19:15:41 +0200

    Update substrings.rb
    main logic

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,25 @@
-
-
-  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"].to_s
-
-  dictionary.reduce(Hash.new(0)) do |result, count|
-    result[count]+=1
-    result 
-  end
-
  
 
+  def substring(word, dicionary)
+
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+  result = (Hash.new(0)) 
+  dictionary.to_s
+  
+  dictionary.each do |i|
+    if word.include? i
+      result[i] =+1
+    end
+
+  end
+
+puts result
+
+
+  end
+ 
+substring("Howdy partner, sit down! How's it going?", dictionary)
 
 
 
