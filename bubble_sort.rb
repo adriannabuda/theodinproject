c10ad7cc36053514968cf2c7ec5325ec378ce6df
adriannabuda <32776262+adriannabuda@users.noreply.github.com>
2020-06-30 11:50:58 +0200

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,24 @@
 def bubble_sort(array)
 
-array = [4,3,78,2,0,2]
 n = array.length
-return array if array.size <= 1
 
-swap = true
-while swap
-      swap = false
-    (n - 1).times do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[x]
-          swap = true
-end
-      end
-    end
-array
+loop do 
+  swapped = false
+  
+(n-1).times do |i|
+if array[i] > array[i+1]
+  array[i], array[i+1] = array[i+1], array[i]
+  swapped = true
   end
+
+  end
+  break if not swapped
+
+end
+array
+end
+
+puts "Type your array:"
+array = gets.chomp.split(",")
+
+p bubble_sort(array)
